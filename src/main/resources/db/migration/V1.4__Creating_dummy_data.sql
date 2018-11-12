27b1a9a4661574d7dbaf3cfa9ab49aeeb01c29cd
Ne0kropolis <fdederen@gmail.com>
2018-11-12 16:13:57 +0200

    Added Dummy Data For Flyway Database

---
+++ src/main/resources/db/migration/V1.4__Creating_dummy_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO PRICING
+VALUES (NEXT VALUE FOR pricing_id_seq, 1, 10, 'M', 10);
+
+INSERT INTO LOT
+VALUES (NEXT VALUE FOR lot_id_seq, 1, 'Cids Garage', 'Hammerhead', 20);
+
+INSERT INTO TICKET
+VALUES (NEXT VALUE FOR ticket_id_seq, 100, '2018-10-19', '10:15:10', '10:35:45', 30);
